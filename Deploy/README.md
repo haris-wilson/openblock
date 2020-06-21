e697209b33bdd2f4198bd51937d67c54cf970d63
johndavistl <johndavistl6@gmail.com>
2020-06-21 21:54:29 +0530

    final commit for hackathon

---
+++ Deploy/README.md
@@ -0,0 +1,6 @@
+### Deploying smart contracts to Matic Side Chain
+
+The deploying of contracts using Truffle.
+
+On the localhost the deploying was success. But in Matic 'mumbai' test net there was some problems.
+So the smart contract deployed through Remix IDE and Metamask Wallet.
--- openblock/app.js
+++ openblock/app.js
@@ -14,10 +14,7 @@ const web3 = new Web3('https://rpc-mumbai.matic.today')
 
 // web3 object is now connected with Matic's node"));
 
-const abi = [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"record","type":"bytes32"},{"indexed":false,"internalType":"uint256","name":"timestamp","type":"uint256"},{"indexed":false,"internalType":"bool","name":"returnValue","type":"bool"}],"name":"CertificateIssued","type":"event"},{"constant":false,"inputs":[{"internalType":"bytes32","name":"_hash","type":"bytes32"}],"name":"issueBadge","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"name":"records","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"bytes32","name":"certificate","type":"bytes32"}],"name":"verifyBadges","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"}
-
-let web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider(etherUrl));
+const abi = [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"record","type":"bytes32"},{"indexed":false,"internalType":"uint256","name":"timestamp","type":"uint256"},{"indexed":false,"internalType":"bool","name":"returnValue","type":"bool"}],"name":"CertificateIssued","type":"event"},{"constant":false,"inputs":[{"internalType":"bytes32","name":"_hash","type":"bytes32"}],"name":"issueBadge","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"name":"records","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"bytes32","name":"certificate","type":"bytes32"}],"name":"verifyBadges","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"}];
 
 
 /*Call the function which already deployed on ethereum network
@@ -27,7 +24,7 @@ const app = express();
 
 function issue(hash){
 	try {
-      await Instance.issueCertificate(hash);
+     Instance.issueCertificate(hash);
     
     } catch(error) {
       console.log("issue failed");
@@ -39,7 +36,7 @@ function verify(hash){
 
     try {
 
-      const verified = await Instance.verifyCertificate(hash);
+      const verified = Instance.verifyCertificate(hash);
       
       assert.equal(verified, true)
     } catch(error) {
