2a012621d8d1b1fc00073850dfb9d806dd791d40
johndavistl <johndavistl6@gmail.com>
2020-06-21 16:10:27 +0530

    added contracts

---
+++ contracts/Issuer.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.5.0;
+
+
+contract Issuer{
+    address public owner;
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+}
+
+
